fde23d4beada238977add95f1abb18889b4c57e0
Andersom Rodrigues <andersom.rfd@hotmail.com>
2024-03-15 22:25:34 -0300

    Valida se o user tem a role da rota

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Events\MyEvent;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/orders', [OrderController::class, 'store'])->middleware('role:customer');
 });
 
 Route::get('my-event', function () {
